4171bf0b9e3db80d971fd77debd0aa40e79cb330
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-06-19 23:07:24 +0530

    Ability To Retrieve Employee Details Joined In Particular Date Range

--- src/test/java/com/magic/employeepayroll/PayRollConnectionTest.java
+++ src/test/java/com/magic/employeepayroll/PayRollConnectionTest.java
@@ -34,4 +34,13 @@ public class PayRollConnectionTest {
                     resultSet.getString(12));
         }
     }
+    @Test
+    public void toRetrieve_details_OfEmployee_inParticular_dateRange() throws SQLException{
+        Connection dbConnection = new PayRollConnection().getDBConnection();
+        Statement statement = dbConnection.createStatement();
+        ResultSet resultSet = statement.executeQuery("Select * from employee_pay_roll where Start_Date BETWEEN '2020-10-11' AND '2021-06-19'");
+        while(resultSet.next()){
+            System.out.println(resultSet.getString("Name"));
+        }
+    }
 }
